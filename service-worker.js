00af4c0448d4bde6cebdd0b38352e91ad804697d
@UhLukas <119970521+UhLukas@users.noreply.github.com>
2025-06-17 21:19:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,11 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('dasc-cache-v1').then(cache => {
       return cache.addAll([
-        '/Kleversuite-PWA/home',
-        '/Kleversuite-PWA/index.html',
-        '/Kleversuite-PWA/manifest.json',
-        '/Kleversuite-PWA/icons/icon-192x192.png',
-        '/Kleversuite-PWA/icons/icon-512x512.png'
+        '/home',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
       ]);
     })
   );
